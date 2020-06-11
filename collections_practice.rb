372982d8896acb5046f9aa9718e0715fe1f4b1bc
Sarah Peters <sarahpieters15@gmail.com>
2020-06-11 15:12:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,6 +26,13 @@ end
 
 def kesha_maker(array)
   array = ["blake", "ashley", "scott"]
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
 end
 
 def find_a(array)
